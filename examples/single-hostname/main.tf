bfd2bc67af9849cc4ca4dafc3383585f5028747d
Benjamin Brouard <bbrouard@akamai.com>
2023-03-10 14:46:59 +0100

    Edit example

--- examples/single-hostname/main.tf
+++ examples/single-hostname/main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
-    linode = {
-      source = "linode/linode"
+    akamai = {
+      source  = "akamai/akamai"
+      version = ">= 3.2.0"
     }
   }
-  # Constraint to specify which versions of Terraform can be used with this configuration.
   required_version = ">= 1.0.0"
 }
 
--- examples/single-hostname/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    akamai = {
-      source  = "akamai/akamai"
-      version = ">= 3.2.0"
-    }
-  }
-  required_version = ">= 1.0.0"
-}
